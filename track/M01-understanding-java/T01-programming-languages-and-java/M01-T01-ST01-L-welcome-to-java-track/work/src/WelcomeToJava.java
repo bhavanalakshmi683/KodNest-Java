51ef6c8131f8cd4cdb17c79ae7225013701d27ad
bhavanalakshmi683 <bhavanalakshmi683@gmail.com>
2026-07-30 18:50:30 +0530

    Day1Challenges

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,11 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       //Write your code here
-       System.out.println("Welcome to JAVA Track");    }
+        //Write your code here
+        System.out.println("Welcome to JAVA Track");
+        System.out.println("I already know how to build programming logic");
+        System.out.println("I am looking to become a professional programmer");
+        System.out.println("I am confident that i will be able to achieve my goal ");
+    }
 }
