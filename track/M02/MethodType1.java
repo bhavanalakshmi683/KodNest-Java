81614192b5f31757df060544de4591a41a7f55f0
bhavanalakshmi683 <bhavanalakshmi683@gmail.com>
2026-08-21 19:37:04 +0530

    Methods all 4 types

---
+++ track/M02/MethodType1.java
@@ -0,0 +1,10 @@
+class MethodType1 {
+
+    public static void main(String[] args) {
+        greet();
+    }
+
+    static void greet() {
+        System.out.println("Good Morning");
+    }
+}
---
+++ track/M02/MethodeType2.java
@@ -0,0 +1,13 @@
+
+class MethodType2 {
+
+    public static void main(String[] args) {
+        add(10, 5);
+    }
+
+    static void add(int a, int b) {
+        int res = a + b;
+
+        System.out.println(res);
+    }
+}
